04a947eba98218d95fedcbf500fd8bfbe6e781c6
dgusfr <diegogustavo.fr@gmail.com>
2024-09-11 18:40:10 -0300

    feat(header): Adicionado componente Header com links de navegação

---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <p>© 2024 Receitas da Sonia. Todos os direitos reservados.</p>
+      <ul>
+        <li>
+          <a href="#">Política de Privacidade</a>
+        </li>
+        <li>
+          <a href="#">Termos de Serviço</a>
+        </li>
+      </ul>
+    </footer>
+  );
+}
+
+export default Footer;
