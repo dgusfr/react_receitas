4145e1c77cc8a032402e4abe0b1689d7a50cedb8
dgusfr <diegogustavo.fr@gmail.com>
2024-09-13 08:12:49 -0300

    feat(app): import

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./Components/Header/Header";
-import Footer from "./Components/Footer/footer";
+import Footer from "./Components/Footer/Footer";
 import Home from "./Page/Home/home";
 import Categorias from "./Page/Categorias/categorias";
 
--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import styles from "./header.module.css"; // Importa os estilos
+import styles from "./header.module.css";
 
 function Header() {
   return (
