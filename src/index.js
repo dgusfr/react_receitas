1e5797b7f1e96379d021c53c9721344e9e200208
dgusfr <diegogustavo.fr@gmail.com>
2024-09-22 12:13:56 -0300

    fix: update index.js to use createRoot instead of ReactDOM.render for React 18 compatibility

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import "./index.css";
-import Routes from "./routes";
+import RoutesConfig from "./routes";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
-    <Routes />
-  </React.StrictMode>,
-  document.getElementById("root")
+    <RoutesConfig />
+  </React.StrictMode>
 );
