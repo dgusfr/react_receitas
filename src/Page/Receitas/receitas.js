5ede947aa9b6dd16cf9defc5a8eaed209f04a450
dgusfr <diegogustavo.fr@gmail.com>
2024-09-27 21:18:17 -0300

    feat(styles): changes styles in recipes

--- src/Page/Receitas/receitas.js
+++ src/Page/Receitas/receitas.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Receitas from "../../components/Receitas/Receitas";
+import Footer from "../../components/Footer/Footer";
 import styles from "./receitas.module.css";
 
 function ReceitasPage() {
   return (
-    <main className={styles.page}>
-      <Receitas />
-    </main>
+    <div className={styles.pageContainer}>
+      <main className={styles.page}>
+        <Receitas />
+      </main>
+    </div>
   );
 }
 
